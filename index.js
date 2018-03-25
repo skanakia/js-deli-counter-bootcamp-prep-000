44fc633a4e3b823eb82ff42c35c3dd08a4b10a56
Sameer Kanakia <skanakia1@gmail.com>
2018-03-25 15:45:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(arr) {
+  if (arr.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else { 
+    return 'Currently serving ' + arr.shift() + "."
+  }
+}
+
+function currentLine()
